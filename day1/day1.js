607030d447047bb174659cd0a48d69c00b4d3a20
Gaddam Hima <124161943+hima1202@users.noreply.github.com>
2023-07-14 22:16:23 +0530

    Update day1.js

--- day1/day1.js
+++ day1/day1.js
@@ -1,31 +1,47 @@
 function numbersThenCharacters(arr) {
-  const sortedArr = arr.map(subArr => {
-    const numbers = [];
-    const letters = [];
+  const numbers = [];
+  const letters = [];
 
-    subArr.forEach(element => {
+  
+  for (let i = 0; i < arr.length; i++) {
+    const subArr = arr[i];
+    for (let j = 0; j < subArr.length; j++) {
+      const element = subArr[j];
       if (typeof element === 'number') {
         numbers.push(element);
       } else if (typeof element === 'string') {
         letters.push(element);
       }
-    });
+    }
+   }
+  
 
-    numbers.sort((a, b) => a - b);
-    letters.sort();
-
-    return [...numbers, ...letters];
-  });
-
-  return sortedArr;
+  numbers.sort(function(a,b){return a-b});
+  letters.sort();
+  const mergedSubArr = [...numbers, ...letters];
+  
+  
+  const sortedArr=[];
+   for(let i=0;i<arr.length;i++){
+       const subArr1=[];
+       const l=arr[i].length;
+       for(let j=0;j<l;j++)
+       {
+           const element=mergedSubArr.shift();
+           subArr1.push(element);
+       }
+       sortedArr.push(subArr1);
+   }
+ return sortedArr;
 }
-
-const arr = [
-  [11, 12, 24, 23, 'a', 'b'],
-  [56, 'c', 45],
-  [67, 'd'],
-  ['f', 'e', 78]
-];
-
-const sortedArr = numbersThenCharacters(arr);
-console.log(sortedArr);
+const arr=[
+[11, 26, 45.4, "f", "a", "b"],
+[0], [1.5, "d","X",35,"s"],
+["f", "e", 58],
+["p","Y","Z"],
+[98,72]
+]
+ 
+ sortedArr = numbersThenCharacters(arr);
+  console.log(sortedArr);
+   
